ececcdf4d6c7e60c5c0e5284b3ed3bea70369337
EstherAkuosnaba <akuosnabaesther@gmail.com>
2020-12-02 16:53:12 +0000

    firstcommit

--- src/Container/Login.js
+++ src/Container/Login.js
@@ -24,7 +24,7 @@ function Login() {
                     <span class="loginwith">Sign in with<br />social network</span>
 
                     <button class="social-signin facebook">Signin with facebook <i class='bx bxl-facebook-circle'></i></button>
-                    <button class="social-signin twitter"><i class='bx bxl-twitter'></i>signin with twitter </button>
+                    <button class="social-signin twitter">signin with twitter<i class='bx bxl-twitter'></i> </button>
                     <button class="social-signin google">signin with google<box-icon type='logo' name='google'></box-icon></button>
                     <button id="dee" class="social-signin instagram">signin with instagram<box-icon name='instagram' type='logo' ></box-icon></button>
                     <span>
